8533e73e93bae10d3a9dd74b255fcccc14ecc0c1
planeman1123 <ianarchibald897@gmail.com>
2013-11-03 15:35:10 -0600

    Clean-up
    Cleaned up the code

--- babylauncher/lua/weapons/babylauncher.lua
+++ babylauncher/lua/weapons/babylauncher.lua
@@ -26,101 +26,52 @@ SWEP.DrawCrosshair		= true
 
 SWEP.ViewModel			= "models/weapons/v_rpg.mdl"
 SWEP.WorldModel			= "models/weapons/w_rocket_launcher.mdl"
---
--- Called when the left mouse button is pressed
---
+
 function SWEP:PrimaryAttack()
 
-	-- This weapon is 'automatic'. This function call below defines
-	-- the rate of fire. Here we set it to shoot every 0.5 seconds.
 	self.Weapon:SetNextPrimaryFire( CurTime() + 0.1 )	
 
-	-- Call 'ThrowChair' on self with this model
+
 	self:ThrowBaby( "models/props_c17/doll01.mdl" )
 
 end
  
 
---
--- Called when the rightmouse button is pressed
---
 
 local ShootSound = Sound( "Weapon_mac10.Single" )
 
 function SWEP:SecondaryAttack()
-
-	-- Note we don't call SetNextSecondaryFire here because it's not
-	-- automatic and so we let them fire as fast as they can click.	
+	
 	self.Weapon:SetNextSecondaryFire( CurTime() + 0.1 )	
-	-- Call 'ThrowChair' on self with this model
+
 	self:ThrowMelon( "models/props_junk/watermelon01.mdl" )
 
 end
 
---
--- A custom function we added. When you call this the player will fire a chair!
---
 function SWEP:ThrowBaby( model_file )
 
-	-- 
-	-- Play the shoot sound we precached earlier!
-	--
 	self:EmitSound( ShootSound )
  
-	--
-	-- If we're the client ) then this is as much as we want to do.
-	-- We play the sound above on the client due to prediction.
-	-- ( if ( we didn't they would feel a ping delay during multiplayer )
-	--
 	if ( CLIENT ) then return end
 
-	--
-	-- Create a prop_physics entity
-	--
 	local ent = ents.Create( "prop_physics" )
 
-	--
-	-- Always make sure that created entities are actually created!
-	--
 	if (  !IsValid( ent ) ) then return end
 
-	--
-	-- Set the entity's model to the passed in model
-	--
 	ent:SetModel( model_file )
- 
-	--
-	-- Set the position to the player's eye position plus 16 units forward.
-	-- Set the angles to the player'e eye angles. Then spawn it.
-	--
+
 	ent:SetPos( self.Owner:EyePos() + ( self.Owner:GetAimVector() * 16 ) )
 	ent:SetAngles( self.Owner:EyeAngles() )
 	ent:Spawn()
- 
 
-	--
-	-- Now get the physics object. Whenever we get a physics object
-	-- we need to test to make sure its valid before using it.
-	-- If it isn't ) then we'll remove the entity.
-	--
 	local phys = ent:GetPhysicsObject()
 	if (  !IsValid( phys ) ) then ent:Remove() return end
- 
- 
-	--
-	-- Now we apply the force - so the chair actually throws instead 
-	-- of just falling to the ground. You can play with this value here
-	-- to adjust how fast we throw it.
-	--
+
 	local velocity = self.Owner:GetAimVector()
 	velocity = velocity * 1000000000
 	velocity = velocity + ( VectorRand() * 100 ) -- a random element
 	phys:ApplyForceCenter( velocity )
- 
-	--
-	-- Assuming we're playing in Sandbox mode we want to add this
-	-- entity to the cleanup and undo lists. This is done like so.
-	--
+
 	cleanup.Add( self.Owner, "props", ent )
  
 	undo.Create( "your cumstain" )
@@ -131,66 +82,28 @@ end
 
 function SWEP:ThrowMelon( model_file )
 
-	-- 
-	-- Play the shoot sound we precached earlier!
-	--
 	self:EmitSound( ShootSound )
 
- 
-	--
-	-- If we're the client ) then this is as much as we want to do.
-	-- We play the sound above on the client due to prediction.
-	-- ( if ( we didn't they would feel a ping delay during multiplayer )
-	--
 	if ( CLIENT ) then return end
 
-	--
-	-- Create a prop_physics entity
-	--
 	local ent = ents.Create( "prop_physics" )
 
-	--
-	-- Always make sure that created entities are actually created!
-	--
 	if (  !IsValid( ent ) ) then return end
 
-	--
-	-- Set the entity's model to the passed in model
-	--
 	ent:SetModel( model_file )
  
-	--
-	-- Set the position to the player's eye position plus 16 units forward.
-	-- Set the angles to the player'e eye angles. Then spawn it.
-	--
 	ent:SetPos( self.Owner:EyePos() + ( self.Owner:GetAimVector() * 16 ) )
 	ent:SetAngles( self.Owner:EyeAngles() )
 	ent:Spawn()
- 
 
-	--
-	-- Now get the physics object. Whenever we get a physics object
-	-- we need to test to make sure its valid before using it.
-	-- If it isn't ) then we'll remove the entity.
-	--
 	local phys = ent:GetPhysicsObject()
 	if (  !IsValid( phys ) ) then ent:Remove() return end
- 
- 
-	--
-	-- Now we apply the force - so the chair actually throws instead 
-	-- of just falling to the ground. You can play with this value here
-	-- to adjust how fast we throw it.
-	--
+
 	local velocity = self.Owner:GetAimVector()
 	velocity = velocity * 10000000000000000000000
 	velocity = velocity + ( VectorRand() * 100 ) -- a random element
 	phys:ApplyForceCenter( velocity )
- 
-	--
-	-- Assuming we're playing in Sandbox mode we want to add this
-	-- entity to the cleanup and undo lists. This is done like so.
-	--
+
 	cleanup.Add( self.Owner, "props", ent )
  
 	undo.Create( "Thrown Melon" )
